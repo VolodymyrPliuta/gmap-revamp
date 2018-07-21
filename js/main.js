2c3e78ee5e2e767484c75af5d0a713e56362725d
americanman <snowboardracer7@gmail.com>
2018-07-21 14:01:02 -0400

    Refactor createMap function, move zoom and city in global scope, pass zoom and city in createMap as arguments

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 function initMap() {
-  let map = createMap()
+  let Jacksonville = {lat: 30.274438, lng: -81.388347};
+  let mapZoom = 5;
+  let map = createMap(mapZoom, Jacksonville);
 }
 
-function createMap() {
+function createMap(zoom, city) {
   return(
     new google.maps.Map(document.getElementById('map'), {
-      zoom: 5,
-      center: {lat: 30.274438, lng: -81.388347},
+      zoom: zoom,
+      center: city
     })
   )
 }
