add736aedd0691c7358bbacfda8d5ba038be1bde
americanman <snowboardracer7@gmail.com>
2018-07-21 14:21:36 -0400

    Add SanFrancisco to cities, add values to SF object

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 function initMap() {
-  let Jacksonville = {city: {lat: 30.274438, lng: -81.388347}, zoom: 5}
-  let map = createMap(Jacksonville);
+  let cities = {
+    Jacksonville: {city: {lat: 30.274438, lng: -81.388347}, zoom: 5},
+    SF: {cityName: 'SanFrancisco', city: {lat: 37.761201, lng: -122.434701}, zoom: 5}
+  }
+  let map = createMap(cities.SF);
 }
 
 function createMap({ city, zoom}) {
