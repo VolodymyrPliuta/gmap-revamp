a95459118e00656ed8f34eeaf129bac7e7756084
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-07-21 14:38:52 -0400

    input the stuff.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 function initMap() {
   let cities = usCities;
+  
   let map = createMap(cities.SF);
+  let neighborhoods = usNeighborhoods   
+  let markers = [];
+
+  markers = mapTheMarkers(map, neighborhoods, markers)
 }
 
 function createMap({ city, zoom}) {
@@ -12,6 +17,24 @@ function createMap({ city, zoom}) {
     })
   )
 }
+
+function mapTheMarkers(map, neighborhoods, markers) {
+  return markers.push(
+    newMarker(neighborhoods, map)
+  )
+}
+
+function newMarker(neighborhoods, map){
+  return new google.maps.Marker({
+    position: neighborhoods[0],
+    map: map}
+  )
+}
+
+const usNeighborhoods = [
+  {lat: 30.274438, lng:  -81.388347},
+  {lat: 28.546863, lng: -81.373917}
+];
 
 const usCities = {
   Jacksonville: {city: {lat: 30.274438, lng: -81.388347}, zoom: 5},
