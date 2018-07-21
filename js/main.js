24b0606e6c35f0020b6ab819db1909bf815a4754
americanman <snowboardracer7@gmail.com>
2018-07-21 14:13:53 -0400

    Refactor the initMap function, created an city object, implement destrycturing in the createMap function

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 function initMap() {
-  let Jacksonville = {lat: 30.274438, lng: -81.388347};
-  let mapZoom = 5;
-  let map = createMap(mapZoom, Jacksonville);
+  let Jacksonville = {city: {lat: 30.274438, lng: -81.388347}, zoom: 5}
+  let map = createMap(Jacksonville);
 }
 
-function createMap(zoom, city) {
+function createMap({ city, zoom}) {
   return(
-    new google.maps.Map(document.getElementById('map'), {
-      zoom: zoom,
-      center: city
+    new google.maps.Map(document.getElementById('map'),
+    {
+      center: city,
+      zoom: zoom
     })
   )
 }
