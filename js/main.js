d17cefef780c0664ee5e6d28604bd624b546b4d9
americanman <snowboardracer7@gmail.com>
2018-07-21 13:22:03 -0400

    Refactor initMap moving createMap logic into own function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 5,
-          center: {lat: 30.274438, lng: -81.388347},
-        });
-      }
+function initMap() {
+  let map = createMap()
+}
+
+function createMap() {
+  return(
+    new google.maps.Map(document.getElementById('map'), {
+      zoom: 5,
+      center: {lat: 30.274438, lng: -81.388347},
+    })
+  )
+}
